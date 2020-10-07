936c32d982527147b3e46bed7ae8215d0165846d
PolinaEfimova888 <57696453+PolinaEfimova888@users.noreply.github.com>
2020-10-07 18:36:41 +0800

    Create task2.py

---
+++ task2.py
@@ -0,0 +1,38 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def upload_img(filename):
+    s = []
+    lines = open(filename)
+    lines = lines.readlines()
+
+    for lin in lines[2:]:
+        s.append(lin.split())
+
+    return np.array(s, dtype='int32')
+
+img1 = upload_img('img1.txt')
+img2 = upload_img('img2.txt')
+
+
+def find_first_point(img):
+    for y in range(img.shape[0]):
+        for x in range(img.shape[1]):
+            if img[y, x] == 1:
+                return y, x
+
+y1, x1 = find_first_point(img1)
+y2, x2 = find_first_point(img2)
+
+x=x2-x1
+y=y2-y1
+
+print(f'смещение\nх = {x}\nу = {y}')
+
+plt.figure()
+plt.subplot(121)
+plt.imshow(img1)
+plt.subplot(122)
+plt.imshow(img2)
+plt.show()
