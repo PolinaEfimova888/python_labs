11168ee47a3159c56924719961e6d28ac2ebce66
PolinaEfimova888 <57696453+PolinaEfimova888@users.noreply.github.com>
2020-10-15 15:13:27 +0800

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,17 +1,32 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-for i in range(1,7):
+def getNominalResolution(image,realSize):
+    lst=[]
+    lst.append(np.where(image==1))
+    max=0
+    for i in range(len(lst[0][0])):
+        for j in range(len(lst[0][0])):
+            if i!=j:
+                temp=int((lst[0][0][i]-lst[0][0][j])**2 + (lst[0][1][i]-lst[0][1][j])**2)**0.5
+                if temp>max: max=temp
 
+    if max==0:
+        return "The figure does not exist"
+    else:            
+        return (realSize/max)
+
+
+for i in range(1,7):
     name = 'figure'
     txt = '.txt'
 
     file = open(name + str(i) + txt)
-    hor_size = float(file.readline())
-   
-    lines = file.readlines()[1:]
-    length = len(lines[0].split())
+    realSize=float(file.readline())
+    file.close()
+    img = np.genfromtxt(fname=name + str(i) + txt,skip_header=1)
 
-    a = hor_size/length
 
-    print(f'Answer for figure{i}.txt = {a}\n')
+    getNominalResolution(img, realSize)
+    print(f'Answer for figure{i}.txt:')
+    print(getNominalResolution(img, realSize))
